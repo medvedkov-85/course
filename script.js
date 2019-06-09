e27a03226e6743647400688b4d7e1f121f119ba3
Dmitry <medvedkov-85@yandex.ru>
2019-06-09 12:25:33 +0300

    Выполнение задания курса

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-'use strict';
-let a = 5;
-console.log(a);
+(function () {
+    'use strict';
+    // this function is strict...
+ }());
+let money = prompt('Ваш бюджет на месяц');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+
+let appData = {
+    budget: money,
+    timedata: time,
+    expences: {},
+    optionalExpenses:{},
+    income:[],
+    savings:false
+};
+
+for(let i = 0; i < 2; i++) {
+    let key = prompt('Введите обязательную статью расходов в этом месяце');
+    let value = prompt('Во сколько обойдется?');
+    appData.expences[key] = value;
+}
+console.log(appData);
